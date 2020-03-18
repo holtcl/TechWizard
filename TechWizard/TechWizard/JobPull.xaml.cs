f5888f879af4fdbc554bc0bfdbf4db3c3474168e
holtcl <holtcl84@gmail.com>
2020-03-17 21:40:36 -0400

    Added features to Jobs Pull page

--- TechWizard/TechWizard/JobPull.xaml.cs
+++ TechWizard/TechWizard/JobPull.xaml.cs
@@ -3,6 +3,9 @@ using System.Net.Http;
 using TechWizard.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace TechWizard
 {
@@ -82,10 +85,31 @@ namespace TechWizard
                 contactContactTypeLabel.Text = thisRequest.contactMethod;
                 contactFirstName.Text = contact.FirstName;
 
-                if (thisRequest.contactMethod == "Phone") contactContactInfo.Text = contact.Phone;
-                else if (thisRequest.contactMethod == "Email") contactContactInfo.Text = contact.Email;
-                else if (thisRequest.contactMethod == "In-Person") contactContactInfo.Text = contact.Address +
-                        "\n" + contact.City + ", " + contact.State + " " + contact.Zip;
+                if (thisRequest.contactMethod == "Phone")
+                {
+                    contactContactInfo.Text = contact.Phone;
+                    phoneButton.IsVisible = true;
+
+                }
+                else if (thisRequest.contactMethod == "Text")
+                {
+                    contactContactInfo.Text = contact.Phone;
+                    textButton.IsVisible = true;
+                }
+                else if (thisRequest.contactMethod == "Email")
+                {
+                    contactContactInfo.Text = contact.Email;
+                    emailButton.IsVisible = true;
+                }
+
+                else if (thisRequest.contactMethod == "In-Person")
+                {
+                    contactContactInfo.Text = contact.Address +
+                        "\n" + contact.City + ", " + contact.State + " " + contact.Zip +
+                        "\n" + contact.Phone;
+                    phoneButton.IsVisible = true;
+                    GPSButton.IsVisible = true;
+                }
             }
             else 
             {
@@ -117,10 +141,137 @@ namespace TechWizard
                 {
                     await DisplayAlert("Job Accepted!", "Check your orders page for the job info!", "Ok");
                     acceptButton.IsVisible = false;
-                } else
+                } 
+                else
                 {
                     await DisplayAlert("Error!", "Something went wrong processing your request.  Please try again later", "Ok");
                 }
+            }
+        }
+
+        private async void GPSButton_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var placemark = new Placemark
+                {
+
+                    Thoroughfare = contact.Address,
+                    Locality = contact.City,
+                    AdminArea = contact.State,
+                    PostalCode = contact.Zip.ToString()
+                };
+
+                await Map.OpenAsync(
+
+                    placemark, new MapLaunchOptions
+                    { NavigationMode = NavigationMode.Driving });
+                {
+
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                await DisplayAlert("Faild", fnsEx.Message, "OK");
+            }
+            catch (PermissionException pEx)
+            {
+                await DisplayAlert("Faild", pEx.Message, "OK");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Faild", ex.Message, "OK");
+            }
+        }
+    
+
+        private async void textButton_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(contact.Phone))
+            {
+                await SendSms("Hello " + contact.FirstName + " this is your TechWizard here to help you with your issue " + thisRequest.title, contact.Phone);
+            }
+        }
+
+        private async void phoneButton_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(contact.Phone))
+            {
+                await Call(contact.Phone);
+            }
+        }
+
+        private async void emailButton_Clicked(object sender, EventArgs e)
+        {
+            {
+                List<string> toAddress = new List<string>();
+                toAddress.Add(contact.Email);
+                await SendEmail(thisRequest.title, "Hello " +contact.FirstName + "," + "\n", toAddress);
+            }
+        }
+
+        public async Task SendEmail(string subject, string body, List<string> recipients)
+        {
+            try
+            {
+                var message = new EmailMessage
+                {
+                    Subject = subject,
+                    Body = body,
+                    To = recipients,
+                };
+                await Email.ComposeAsync(message);
+            }
+            catch (FeatureNotSupportedException fbsEx)
+            {
+                await DisplayAlert("Failed", "Email is not supported on this device.", "OK");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Failed", ex.Message, "OK");
+            }
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+
+
+        public async Task Call(string number)
+        {
+            try
+            {
+                PhoneDialer.Open(number);
+            }
+            catch (ArgumentNullException anEx)
+            {
+                await DisplayAlert("Not Valid", "Number Entered was not valid", "Failed");
+            }
+            catch (FeatureNotSupportedException ex)
+            {
+                await DisplayAlert("Not Supported", "Phone Dialer is not supported on this device", "Failed");
+            }
+            catch (System.Exception ex)
+            {
+                await DisplayAlert("???", "Something else has happened", "Failed");
+            }
+        }
+
+        public async Task SendSms(string messageText, string recipient)
+        {
+            try
+            {
+                var message = new SmsMessage(messageText, recipient);
+                await Sms.ComposeAsync(message);
+            }
+            catch (FeatureNotSupportedException ex)
+            {
+                await DisplayAlert("Failed", "Sms is not supported on this device.", "OK");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Failed", ex.Message, "OK");
             }
         }
     }
